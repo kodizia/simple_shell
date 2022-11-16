e4e4716c64166858a64ca61e45cb8e80b674dabf
christian <horpeyemijoshua@gmail.com>
2022-11-16 15:20:48 +0100

    shell solution

--- tokenizer.c
+++ tokenizer.c
@@ -78,17 +78,17 @@ char **strtow2(char *str, char d)
 		while (str[i + k] != d && str[i + k] && str[i + k] != d)
 			k++;
 		s[j] = malloc((k + 1) * sizeof(char));
-if (!s[j])
-{
-for (k = 0; k < j; k++)
-free(s[k]);
-free(s);
-return (NULL);
-}
-for (m = 0; m < k; m++)
-s[j][m] = str[i++];
-s[j][m] = 0;
-}
-s[j] = NULL;
-return (s);
+		if (!s[j])
+		{
+			for (k = 0; k < j; k++)
+				free(s[k]);
+			free(s);
+			return (NULL);
+		}
+		for (m = 0; m < k; m++)
+			s[j][m] = str[i++];
+		s[j][m] = 0;
+	}
+	s[j] = NULL;
+	return (s);
 }
